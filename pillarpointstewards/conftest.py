572a7c1572920e3b2ea35a5c44a7f2bdc045df02
Simon Willison <swillison@gmail.com>
2022-03-30 14:02:19 -0700

    Test for upcoming_shifts, refs #8

--- pillarpointstewards/conftest.py
+++ pillarpointstewards/conftest.py
@@ -1,4 +1,7 @@
+import datetime
 import pytest
+from django.utils.timezone import make_aware
+import pytz
 
 
 @pytest.fixture(autouse=True)
@@ -13,4 +16,18 @@ def configure_whitenoise(settings):
     # https://github.com/natbat/pillarpointstewards/issues/16#issuecomment-1071440848
     settings.STATICFILES_STORAGE = (
         "django.contrib.staticfiles.storage.StaticFilesStorage"
+    )
+
+
+@pytest.fixture
+def admin_user_has_shift(admin_user):
+    dt = make_aware(
+        datetime.datetime.utcnow() + datetime.timedelta(hours=96),
+        timezone=pytz.timezone("America/Los_Angeles"),
+    )
+    admin_user.shifts.create(
+        shift_start=dt,
+        shift_end=dt + datetime.timedelta(hours=2),
+        dawn=dt - datetime.timedelta(hours=1),
+        dusk=dt - +datetime.timedelta(hours=5),
     )
--- pillarpointstewards/homepage/tests.py
+++ pillarpointstewards/homepage/tests.py
@@ -9,3 +9,9 @@ def test_homepage_logged_in(admin_client):
     response = admin_client.get("/")
     html = response.content.decode("utf-8")
     assert ">Coming soon!<" not in html
+    assert response.context["upcoming_shifts"] == []
+
+
+def test_homepage_shows_assigned_shifts(admin_client, admin_user_has_shift):
+    response = admin_client.get("/")
+    assert len(response.context["upcoming_shifts"]) == 1
