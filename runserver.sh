12b2752cd4cc720d03af550490cb335684c5e556
Natalie Downe <nat@natbat.net>
2022-04-01 12:31:32 -0700

    Use DJANGO_DEBUG for runserver

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export DATABASE_URL="postgresql://localhost/pillarpointstewards"
+export DJANGO_DEBUG=1
 ./pillarpointstewards/manage.py migrate
 ./pillarpointstewards/manage.py runserver
