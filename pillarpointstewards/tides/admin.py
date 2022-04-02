68e6ffa73dc03a7f0201a98c31d8f63d5b91fde7
Simon Willison <swillison@gmail.com>
2022-04-02 16:30:33 -0700

    Admin view for tide predictions, refs #44

--- pillarpointstewards/tides/admin.py
+++ pillarpointstewards/tides/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TidePrediction
 
-# Register your models here.
+admin.site.register(
+    TidePrediction,
+    list_display=("station_id", "dt", "mllw_feet"),
+    readonly_fields=("station_id", "dt", "mllw_feet"),
+)
