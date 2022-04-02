33b58dd763814f62ca7351e967669fe287b64047
Simon Willison <swillison@gmail.com>
2022-04-02 14:43:10 -0700

    Improvements to secret link admin, refs #50

--- pillarpointstewards/auth0_login/admin.py
+++ pillarpointstewards/auth0_login/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Auth0User, ActiveUserSignupLink
 
 admin.site.register(Auth0User)
-admin.site.register(ActiveUserSignupLink, readonly_fields=("signup_url",))
+admin.site.register(
+    ActiveUserSignupLink,
+    readonly_fields=("secret", "signup_url", "users_created_with_this_link"),
+    exclude=("created_users",),
+    list_display=("secret", "is_active", "comment", "users_created_with_this_link")
+)
--- pillarpointstewards/auth0_login/models.py
+++ pillarpointstewards/auth0_login/models.py
@@ -29,6 +29,15 @@ class ActiveUserSignupLink(models.Model):
         help_text="If not active the link will no longer create activated accounts",
     )
     created_users = models.ManyToManyField("auth.User", related_name="+", blank=True)
+    comment = models.TextField(blank=True, null=True, help_text="Private note about where this URL was shared")
+
+    @property
+    def users_created_with_this_link(self):
+        # Avoid recursion error on /admin/auth0_login/activeusersignuplink/add/
+        if self.pk:
+            return ", ".join(self.created_users.values_list("username", flat=True))
+        else:
+            return ""
 
     @property
     def path(self):
@@ -36,7 +45,10 @@ class ActiveUserSignupLink(models.Model):
 
     @property
     def signup_url(self):
-        return "https://www.pillarpointstewards.com{}".format(self.path)
+        if self.pk:
+            return "https://www.pillarpointstewards.com{}".format(self.path)
+        else:
+            return ""
 
     def __str__(self):
         return f'Secret {self.secret} is {"active" if self.is_active else "inactive"} and has created {self.created_users.count()} users'
