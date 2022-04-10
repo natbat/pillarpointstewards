c495fec72aa7d63a21f7f0ae647073bd4feb96dd
Simon Willison <swillison@gmail.com>
2022-04-10 11:45:27 -0700

    Secret Calendar admin improvements, refs #11

--- pillarpointstewards/shifts/admin.py
+++ pillarpointstewards/shifts/admin.py
@@ -11,6 +11,17 @@ class ShiftAdmin(admin.ModelAdmin):
         return ", ".join(obj.stewards.values_list("username", flat=True))
 
 
+class SecretCalendarAdmin(admin.ModelAdmin):
+    list_display = ("user", "created")
+    readonly_fields = ("secret", "calendar_url")
+    raw_id_fields = ("user",)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ("user",)
+        return self.readonly_fields
+
+
 admin.site.register(Shift, ShiftAdmin)
 admin.site.register(ShiftChange)
-admin.site.register(SecretCalendar, readonly_fields=("secret", "calendar_url"))
+admin.site.register(SecretCalendar, SecretCalendarAdmin)
